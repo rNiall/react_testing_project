f4ce2005f9422e039d44b90ace96aecb3a59cb98
Naill <2jetsetter@gmail.com>
2016-10-17 14:10:13 +0300

    add ContactList and connect it with app.js

---
+++ src/ContactsList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ContactsList extends React.Component {
+	render() {
+		return (
+			<ul>
+				<li>Niall +7 921 123 45 67</li>
+			</ul>
+		)
+	}
+}
+
+export default ContactsList;
